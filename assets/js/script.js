32d3c3b98eb608c4cececd67b948be8483263e76
Cosmin-Ariton <cosminiris12@gmail.com>
2021-04-06 16:23:17 +0200

    js.first

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var nomeUtente = prompt('Qual è il tuo nome?');
+var cognomeUtente = prompt('Qual è il tuo cognome?');
+var colorP = prompt('Qual è il tuo colore preferito?')
+
+document.getElementById('password').innerHTML = 'La tua password è: ' + nomeUtente + cognomeUtente + colorP + 21;
